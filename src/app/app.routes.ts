9bad765a9da95fa7b4f956dc9d28ee3287cfdc60
AbirDaikhi-dev <abirdaikhi.dev@gmail.com>
2024-12-25 19:43:28 +0100

    Add Routes And Components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserDetailsComponent } from './user/user-details/user-details.component';
+import { UserFormComponent } from './user/user-form/user-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'users',
+        pathMatch: 'full',
+    },
+    {
+        path: 'users',
+        component: UserListComponent
+    },
+    {
+        path: 'user/:id',
+        component: UserDetailsComponent 
+    },
+    {
+        path: 'add-user',
+        component: UserFormComponent
+    },
+    {
+        path: 'edit-user/:id',
+        component: UserFormComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'users'
+    }
+];
