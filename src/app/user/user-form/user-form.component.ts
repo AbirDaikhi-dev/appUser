b05df07a6415015380e7c88b42b8c95b04532ba8
AbirDaikhi-dev <abirdaikhi.dev@gmail.com>
2024-12-26 12:21:23 +0100

    Generate User Form Component

--- src/app/user/user-form/user-form.component.ts
+++ src/app/user/user-form/user-form.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, HttpClientModule],
+  providers: [UserService],
   templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.scss'
 })
 export class UserFormComponent {
+  private userService = inject(UserService);
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
 
+  userId = this.route.snapshot.params['id'];
+  userForm = this.fb.group({
+    name: [''],
+    username: [''],
+    email: [''],
+    address: this.fb.group({
+      street: [''],
+      suite: [''],
+      city: [''],
+      zipcode: [''],
+      geo: this.fb.group({
+        lat: [''],
+        lng: [''],
+      }),
+    }),
+    phone: [''],
+    website: [''],
+    company: this.fb.group({
+      name: [''],
+      catchPhrase: [''],
+      bs: [''],
+    }),
+    role: [''],
+  });
+
+  ngOnInit() {
+    if (this.userId) {
+      this.userService.getUser(this.userId).subscribe((user) => {
+        this.userForm.patchValue(user);
+      });
+    }
+  }
+
+  saveUser() {
+    const user = this.userForm.value;
+    if (this.userId) {
+      this.userService.updateUser(this.userId, user).subscribe(() => {
+        this.router.navigate(['/users']);
+      });
+    } else {
+      this.userService.addUser(user).subscribe(() => {
+        this.router.navigate(['/users']);
+      });
+    }
+  }
 }
